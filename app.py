7173e074cb91a3906007a9eadca357d56321345d
J17Bush <115619271+J17Bush@users.noreply.github.com>
2023-09-25 14:41:36 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
+from datetime import timedelta ,datetime
 
 app = Flask(__name__)
 app.secret_key ="hello"
-import routes 
+import routes
+app.permanent_session_lifetime = timedelta(5)
 
 
 if __name__ == "__main__":
--- routes.py
+++ routes.py
@@ -1,7 +1,9 @@
 from __main__ import app 
-from flask import url_for , render_template, redirect, request, flash
+from flask import url_for , render_template, redirect, flash, request, session
 
 from db_connecter import database
+import requests
+import hashlib
 
 #defines our database
 db = database ()
@@ -39,3 +41,65 @@ def add():
     flash("Book Added Successfully")
 
     return redirect(url_for("data"))
+
+@app.route('/delete/<int:Book_ID>', methods=['GET', 'POST'])
+def delete(Book_ID):
+    books = db.queryDB("SELECT * FROM Book_tbl WHERE Book_ID = ?", [Book_ID])
+    if not books:
+        flash('Book not found.', 'danger')
+    else:
+        db.updateDB("DELETE FROM Book_tbl WHERE Book_ID = ?", [Book_ID])
+        flash('Book deleted successfully.', 'success')
+    return redirect(url_for('data'))
+
+@app.route('/Login')
+def login():
+    title = "Log In"
+    found_user = ""
+
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        password = request.form["pword"]
+        hashed_password = hashlib.md5(str(password).encode()).hexdigest()
+        found_user = db.queryDB("SELECT * FROM User_Table WHERE name = ?", [user])
+    if found_user:
+        stored_password = found_user[0][3]
+        if stored_password == hashed_password:
+            session["user"] = user
+            session["email"] = found_user[0][2]
+            flash("LogIn successful", 'success')
+            return redirect(url_for("home"))
+        else:
+            flash("Incorrect password.", "danger")
+    else:
+        flash("User not found.", "danger")
+    
+    if "user" in session:
+        flash("Already Logged IN!", "info")
+        return redirect(url_for("users"))
+    else:
+        return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    current_user = session.get('user')
+    if request.method == "POST":
+        user = request.form["nm"]
+        password = request.form["pword"]
+        email = request.form["email"]
+
+        hashed_email = hashlib.md5(str(email).encode()).hexdigest()
+        hashed_password = hashlib.md5(str(password).encode()).hexdigest()
+
+        result = db.queryDB("SELECT * FROM User_Table WHERE name = ? or email = ?", [user, hashed_email])
+        if result:
+            flash('Email or username already exists, please try a different one', 'danger')
+            return redirect(url_for('register'))
+
+            db.updateDB("INSERT INTO users (name,email,password) VALUES (?,?,?)", [user, hashed_email, hashed_password])
+            return render_template('login.html', title='login')
+        else:
+            return render_template('register.html', title='Register')
+    else:
+        return render_template('register.html', title='Register')
